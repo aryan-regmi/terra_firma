0f68a3a584dfe71a2d2e32ff8a034964c4b2dd03
Aryan Regmi <aryan_regmi@protonmail.com>
2025-06-20 12:46:00 -0600

    Got Tiled map loaded properly

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,17 @@ pub mod button;
 pub mod player;
 pub mod screens;
 
+mod tiled;
+
+pub mod helper {
+    pub use crate::tiled::*;
+}
+
 // TODO: Add egui inspector plugin!
 //
 // TODO: Add loader to load all assets in the beginning!
 //  - Handle when asset isn't fully loaded
 //
 // TODO: Add collisons to map!
+//
+// TODO: Add animation using bevy_ecs_tilemap
--- src/screens/gameplay_screen.rs
+++ src/screens/gameplay_screen.rs
@@ -1,10 +1,11 @@
 use crate::{
+    helper,
     player::{self, Player},
     screens::Screen,
 };
-use avian2d::prelude::*;
+// use avian2d::prelude::*;
 use bevy::{input::common_conditions::input_just_pressed, prelude::*};
-use bevy_ecs_tiled::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
 
 const MAP_SCALE: f32 = 2.0;
 
@@ -12,23 +13,11 @@ const MAP_SCALE: f32 = 2.0;
 #[derive(Component)]
 struct BackgroundMesh;
 
-// Declare a component and make it "reflectable"
-#[derive(Component, Reflect, Default, Debug)]
-#[reflect(Component, Default)]
-struct TileInfo(bool);
-// pub struct TileInfo {
-//     is_collider: bool,
-// }
-
 /// Bundles the systems of the `Gameplay` screen.
 pub(crate) fn plugin(app: &mut App) {
-    app.register_type::<TileInfo>();
     app.add_systems(OnEnter(Screen::Gameplay), (setup, add_colliders).chain());
     app.add_systems(OnExit(Screen::Gameplay), despawn_player);
-    app.add_plugins((
-        TiledMapPlugin::default(),
-        TiledPhysicsPlugin::<TiledPhysicsAvianBackend>::default(),
-    ));
+    app.add_plugins((helper::TiledMapPlugin, TilemapPlugin));
     app.add_systems(
         Update,
         enter_main_screen.run_if(input_just_pressed(KeyCode::Escape)),
@@ -38,37 +27,18 @@ pub(crate) fn plugin(app: &mut App) {
 
 /// Setups up the camera and spawns the map.
 fn setup(mut cmd: Commands, asset_server: Res<AssetServer>) {
-    let map_handle: Handle<TiledMap> = asset_server.load("maps/map_00/main.tmx");
+    let map_handle = helper::TiledMapHandle(asset_server.load("maps/map_00/main.tmx"));
     cmd.spawn((
         StateScoped(Screen::Gameplay),
-        TiledMapHandle(map_handle),
-        TilemapAnchor::Center,
-        TiledPhysicsSettings::<TiledPhysicsAvianBackend> {
-            tiles_layer_filter: TiledName::All,
-            objects_filter: TiledName::All,
-            objects_layer_filter: TiledName::All,
+        helper::TiledMapBundle {
+            tiled_map: map_handle,
+            transform: Transform::default().with_scale(Vec3::splat(MAP_SCALE)),
             ..default()
         },
-        Transform::default().with_scale(Vec3::splat(MAP_SCALE)),
     ));
 }
 
-fn add_colliders(mut cmd: Commands, asset_server: Res<AssetServer>, colliders: Query<&Collider>) {
-    for (i, pos) in colliders.iter().enumerate() {
-        dbg!(i);
-        // if let Some(TileInfo(true)) = info {
-        // cmd.spawn((
-        //     Sprite {
-        //         image: asset_server.load("tileset/character-sprite-sheet.png"),
-        //         ..default()
-        //     },
-        //     RigidBody::Static,
-        //     Collider::rectangle(32.0, 32.0),
-        //     Transform::from_xyz(pos.x as f32, pos.y as f32, 3.0),
-        // ));
-        // }
-    }
-}
+fn add_colliders(mut _cmd: Commands) {}
 
 /// Switches to the main screen.
 fn enter_main_screen(mut next_state: ResMut<NextState<Screen>>) {
